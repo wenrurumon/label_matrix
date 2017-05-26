d3f03e2aa8da9a2cd0f930ae0397e0794e4be4dc
進撃の雯雯老師 <askahu6@gmail.com>
2017-05-26 15:29:14 -0500

    Update full_on_rz.R

--- wuxi/full_on_rz.R
+++ wuxi/full_on_rz.R
@@ -7,12 +7,32 @@ library(sqldf)
 setwd('E:\\无锡线上线下\\201605')
 fd <- dir()
 
+#load data
 fdi <- fd[[1]]
 raw <- lapply(fd,function(fdi){
   setwd('E:\\无锡线上线下\\201605')
   setwd(fdi)
   print(fdi)
+  # fs <- dir()[1:2]
   fs <- dir()
   raw <- lapply(fs,fread,encoding='UTF-8')
   return(raw)
+})
+raw <- do.call(c,raw)
+
+#filter map
+map <- unique(do.call(rbind,lapply(raw,function(x){
+  uid <- as.numeric(x$V1)
+  pid <- x$V4
+  pid <- gsub('【|】(.*)|\\[|\\](.*)','',pid)
+  unique(data.table(uid=uid,pid=pid))
+})))
+uidt <- table(map$uid)
+uidt <- uidt[which(uidt>1)]
+
+#filter raw
+raw2 <- lapply(raw,function(x){
+  v1 <- paste(x$V1)
+  s <- v1%in%names(uidt)
+  subset(x,s)
 })
