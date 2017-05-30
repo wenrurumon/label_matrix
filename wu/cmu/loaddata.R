ab52962266e3632a496fc38249de0c0a2c8cb87c
進撃の雯雯老師 <askahu6@gmail.com>
2017-05-29 23:38:55 -0500

    Update loaddata.R

--- wu/cmu/loaddata.R
+++ wu/cmu/loaddata.R
@@ -6,11 +6,15 @@ library(data.table)
 library(dplyr)
 
 fd <- dir(pattern='2016-05')
-raw <- lapply(fd,function(fdi){
+raw <- lapply(fd[1:5],function(fdi){
   print(fdi)
   setwd('/zfsauton/project/highmark/aska/wx/data')
   setwd(fdi)
-  fs <- dir()
+  fs <- dir()[1:2]
   lapply(fs,fread)
 })
 raw <- do.call(c,raw)
+
+raw2 <- lapply(raw,function(x){
+  
+})
